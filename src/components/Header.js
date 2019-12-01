0f3d9dd07c2ff5aa6926b1bd8846e662a25a909a
faithannong <faith.ong@kcl.ac.uk>
2019-12-01 11:19:19 +0000

    fix navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default () => {
     return(
 
         <div className = "col-xs-12 col-md-12 col-lg-12 header" id="container">
-        <img src={require(`../images/rescu-logo.png`)} alt = "Logo" className="shelter-pic"/>
+        <img src={require(`../images/rescu-logo.png`)} alt = "Logo" className="logo"/>
         <p>Rescuing U from loneliness 😄</p>
         <NavBar className = "navBar"/>
         </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,17 @@
 import React from "react";
 export default () => {
     return(
-        <div className = "navbar" >
-        <div className = "divFill">
+        <div className = "col-xs-12 col-md-12 col-lg-12 navbar" >
+        <div className="divfill">
         <ul>
-        <li><a class="active" href="../homePage" className = "link">Home</a>
-            <img src={require(`../images/paw-icon.png`)} alt = "Logo" className="icon"/></li>
-        <li><a href="../eventPage"  className = "link">Events</a>
-            <img src={require(`../images/calendar-icon.png`)} alt = "Logo" className="icon"/></li>
-        <li><a href="../MatchMe"  className = "link">Match me!</a>
-            <img src={require(`../images/person-icon.png`)} alt = "Logo" className="icon"/></li>
-        <li><a class="active" href="../searchPage" className = "link">Search</a>
-            <img src={require(`../images/search-icon.png`)} alt = "Logo" className="icon"/></li>
+        <li><a class="active" href="../homePage" className = "link">Home<img src={require(`../images/paw-icon.png`)} alt = "Logo" className="icon"/></a>
+            </li>
+        <li><a href="../eventPage"  className = "link">Events<img src={require(`../images/calendar-icon.png`)} alt = "Logo" className="icon"/></a>
+            </li>
+        <li><a href="../MatchMe"  className = "link">Match me!<img src={require(`../images/person-icon.png`)} alt = "Logo" className="icon"/></a>
+            </li>
+        <li><a class="active" href="../searchPage" className = "link">Search<img src={require(`../images/search-icon.png`)} alt = "Logo" className="icon"/></a>
+            </li>
         
         </ul>
         </div>
