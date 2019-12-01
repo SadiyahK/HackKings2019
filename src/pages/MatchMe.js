b45ef1aa31fe235d7473d6740692e2b87308838b
“Zeineb <zeineb.bouchamaoui@kcl.ac.uk>
2019-12-01 06:42:08 +0000

    Started Match me page

--- src/pages/MatchMe.js
+++ src/pages/MatchMe.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import _ from 'lodash';
 
+import NavBar from '../components/NavBar';
 
 export default () => {
   
   return (
     <div className="row">   
+        <NavBar/>
         <div className = "row">
-            <h1 className =""> MATCH ME </h1>
+            <h1 className ="pad-5"> MATCH ME </h1>
+            </div>
+            <div className = "row">
+            <select id="hours">
+            { _.range(1, 51).map(value => <option key={value} value={value}>{value}</option>) }
+           
+            
+          </select>
+         
         </div>
     </div>
   );
--- src/pages/animalResults.js
+++ src/pages/animalResults.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import AnimalSearch from '../components/AnimalSearch';
+import NavBar from '../components/NavBar';
 
 export default () => {
   
   return (
     <div className="row">   
+        <NavBar/>
         <div className="col-xs-5 col-md-5 col-lg-5 margin-5 pad-5 bord">
             <AnimalSearch animalID = "A123456"/>
         </div>
