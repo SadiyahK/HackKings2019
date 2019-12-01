f3dc94926a3acb610f3d324e3e083ba64c1801bd
“Zeineb <zeineb.bouchamaoui@kcl.ac.uk>
2019-12-01 10:37:49 +0000

    Added navigation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,14 +4,15 @@ export default () => {
         <div className = "navbar" >
         <div className = "divFill">
         <ul>
-        <li><a class="active" href="../searchPage" className = "link">Home</a>
-            <img src={require(`../images/search-icon.png`)} alt = "Logo" className="icon"/></li>
-        <li><a href="../events"  className = "link">Events</a>
+        <li><a class="active" href="../homePage" className = "link">Home</a>
+            <img src={require(`../images/person-icon.png`)} alt = "Logo" className="icon"/></li>
+        <li><a href="../eventPage"  className = "link">Events</a>
             <img src={require(`../images/calendar-icon.png`)} alt = "Logo" className="icon"/></li>
         <li><a href="../MatchMe"  className = "link">Match me!</a>
             <img src={require(`../images/paw-icon.png`)} alt = "Logo" className="icon"/></li>
-        <li><a href="#profile"  className = "link">Search</a>
-            <img src={require(`../images/person-icon.png`)} alt = "Logo" className="icon"/></li>
+        <li><a class="active" href="../searchPage" className = "link">Search</a>
+            <img src={require(`../images/search-icon.png`)} alt = "Logo" className="icon"/></li>
+        
         </ul>
         </div>
         </div>
